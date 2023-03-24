89804367a740cd7ab1d33a78ec93a29752e431ce
Ilya Malinowski <i.v.malinovskiy@tinkoff.ru>
2023-03-25 02:43:14 +0300

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import com.google.protobuf.gradle.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+    id("com.google.protobuf") version "0.8.18"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/protobuf")
+        }
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.google.protobuf:protobuf-kotlin:3.22.2")
+    api("io.grpc:grpc-protobuf:1.53.0")
+    api("com.google.protobuf:protobuf-java-util:3.22.2")
+    api("com.google.protobuf:protobuf-kotlin:3.22.2")
+    api("io.grpc:grpc-kotlin-stub:1.3.0")
+    api("io.grpc:grpc-stub:1.53.0")
+    runtimeOnly("io.grpc:grpc-netty:1.53.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.4"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.44.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.2.1:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
