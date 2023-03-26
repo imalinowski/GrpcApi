dc086bd98901d419062d819fa29930d74e189fcd
Ilya Malinowski <i.v.malinovskiy@tinkoff.ru>
2023-03-26 23:55:09 +0300

    draft methods

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,10 +1,11 @@
+import RegistrationServiceGrpcKt.RegistrationServiceCoroutineStub
 import io.grpc.ManagedChannelBuilder
 
 suspend fun main() {
     val port = 50051
 
     val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build()
-    val stub = RegistrationServiceGrpcKt.RegistrationServiceCoroutineStub(channel)
+    val stub = RegistrationServiceCoroutineStub(channel)
     val data = user {
         lastname = "Ivanov"
         firstname = "Petr"
@@ -12,6 +13,12 @@ suspend fun main() {
         age = 23
         gender = Register.User.Gender.MALE
     }
-    val result = stub.register(data)
+    val result = stub.put(data)
+    when {
+        result.succeeded -> { /*TODO*/ }
+        result.error.isNotEmpty() -> { /*TODO*/ }
+        result.isInitialized -> { /*TODO*/ }
+    }
+
     print("Success is ${result.succeeded}")
 }
--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,21 +1,40 @@
+import Register.Result
+import Register.User
+import RegistrationServiceGrpcKt.RegistrationServiceCoroutineImplBase
 import io.grpc.ServerBuilder
 
-private class RegistrationService : RegistrationServiceGrpcKt.RegistrationServiceCoroutineImplBase() {
-    override suspend fun register(request: Register.User): Register.RegistrationResult {
-        print("Registering user ${request.lastname} ${request.firstname} ${request.middlename}, age: ${request.age}, gender: ${request.gender.name}")
-        return registrationResult { succeeded=true }
+private class RegistrationService : RegistrationServiceCoroutineImplBase() {
+    override suspend fun put(request: User): Result {
+        println(
+            "Registering user ${request.lastname} ${request.firstname} " +
+                    "${request.middlename}, age: ${request.age}, gender: ${request.gender.name}"
+        )
+        return result { succeeded = true }
+    }
+
+    override suspend fun getMany(request: Register.Id): Register.ManyUsers {
+        return super.getMany(request)
+    }
+
+    override suspend fun get(request: Register.Id): User {
+        return super.get(request)
+    }
+
+    override suspend fun delete(request: Register.Id): Result {
+        return super.delete(request)
     }
 }
 
 fun main() {
-    val port = System.getenv("PORT")?.toInt() ?: 50051
+    val port = 50051
+    //prepare and run the gRPC web server
     val server = ServerBuilder
         .forPort(port)
         .addService(RegistrationService())
         .build()
     server.start()
+    //shutdown on application terminate
     Runtime.getRuntime().addShutdownHook(Thread {
-        println("Shutdown server")
         server.shutdown()
     })
     //wait for connection until shutdown
